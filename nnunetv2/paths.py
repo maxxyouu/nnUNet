b6f890731ac12ff7c1c4a6f570450e9db43058a3
maxxyouu <max.you@outlook.com>
2024-09-26 10:41:30 -0400

    inference

--- nnunetv2/paths.py
+++ nnunetv2/paths.py
@@ -16,10 +16,11 @@ import os
 
 """
 PLEASE READ paths.md FOR INFORMATION TO HOW TO SET THIS UP
+TODO: REMOVE THE SET ENVIRONMENT VARIABLES
 """
-os.environ['nnUNet_raw']='C:\\Users\\MaxYo\\OneDrive\\Desktop\\nnunet_parent\\nnUNet_raw'
-os.environ['nnUNet_preprocessed']='C:\\Users\\MaxYo\\OneDrive\\Desktop\\nnunet_parent\\nnUNet_preprocessed'
-os.environ['nnUNet_results']='C:\\Users\\MaxYo\\OneDrive\\Desktop\\nnunet_parent\\nnUNet_results'
+# os.environ['nnUNet_raw']='C:\\Users\\MaxYo\\OneDrive\\Desktop\\nnunet_parent\\nnUNet_raw'
+# os.environ['nnUNet_preprocessed']='C:\\Users\\MaxYo\\OneDrive\\Desktop\\nnunet_parent\\nnUNet_preprocessed'
+# os.environ['nnUNet_results']='C:\\Users\\MaxYo\\OneDrive\\Desktop\\nnunet_parent\\nnUNet_results'
 nnUNet_raw = os.environ.get('nnUNet_raw')
 nnUNet_preprocessed = os.environ.get('nnUNet_preprocessed')
 nnUNet_results = os.environ.get('nnUNet_results')
