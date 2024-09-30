6b28796f96e8609bb7822cf3ce7f671ba8146fa6
maxxyouu <max.you@outlook.com>
2024-09-30 11:44:52 -0400

    intensity augmentation working

--- nnunetv2/training/nnUNetTrainer/variants/data_augmentation/nnUNetTrainerDAIntensityAugmentation.py
+++ nnunetv2/training/nnUNetTrainer/variants/data_augmentation/nnUNetTrainerDAIntensityAugmentation.py
@@ -37,6 +37,7 @@ from batchgeneratorsv2.transforms.base.basic_transform import ImageOnlyTransform
 import torch
 import torch.nn.functional as F
 import SimpleITK as sitk
+from scipy.ndimage import convolve
 
 class IntensityAugmentationTransform(ImageOnlyTransform):
     def __init__(self, p_per_channel:float = 1, linear_factor=0.5, window_size=20):
@@ -58,6 +59,33 @@ class IntensityAugmentationTransform(ImageOnlyTransform):
             return img
         augmented_img = self.intensity_augmentation(img, **params)
         return augmented_img
+    
+    def _interp(self, x:  torch.Tensor, xp:  torch.Tensor, fp:  torch.Tensor) ->  torch.Tensor:
+        """One-dimensional linear interpolation for monotonically increasing sample
+        points.
+
+        Returns the one-dimensional piecewise linear interpolant to a function with
+        given discrete data points :math:`(xp, fp)`, evaluated at :math:`x`.
+
+        Args:
+            x: the :math:`x`-coordinates at which to evaluate the interpolated
+                values.
+            xp: the :math:`x`-coordinates of the data points, must be increasing.
+            fp: the :math:`y`-coordinates of the data points, same length as `xp`.
+
+        Returns:
+            the interpolated values, same size as `x`.
+
+        Implementation is drawn from: https://github.com/pytorch/pytorch/issues/50334
+            -should be equivalent to np.interp()
+        """
+        m = (fp[1:] - fp[:-1]) / (xp[1:] - xp[:-1])
+        b = fp[:-1] - (m * xp[:-1])
+
+        indicies = torch.sum(torch.ge(x[:, None], xp[None, :]), 1) - 1
+        indicies = torch.clamp(indicies, 0, len(m) - 1)
+
+        return m[indicies] * x + b[indicies]
 
     def intensity_augmentation(self, image_array: torch.Tensor, **params) -> torch.Tensor:
         """
@@ -67,41 +95,28 @@ class IntensityAugmentationTransform(ImageOnlyTransform):
         if len(params['apply_to_channel']) == 0:
             return image_array
         
-        # return image_array
-        print('here', image_array.shape)
-        
         # Generate random noise curve in PyTorch
-        random_noise = torch.rand(256) * 255
-        # Smooth the random noise curve with a moving average filter (convolution)
-        kernel = torch.ones(self.window_size) / self.window_size
-        kernel = kernel.unsqueeze(0).unsqueeze(0)  # Add batch and channel dimensions for 1D convolution
-        random_noise = random_noise.unsqueeze(0).unsqueeze(0)  # Add batch and channel dimensions
-        smoothed_noise = F.conv1d(random_noise, kernel, padding='reflect').squeeze() # use 'reflect' to ensure the same dimension as the input
+        random_noise = np.random.uniform(0, 255, size=256)
+        kernel = np.ones(self.window_size) / self.window_size
+        smoothed_noise = convolve(random_noise, kernel, mode='reflect')
 
         # Add a random linear component
-        linear_curve_random_factor = torch.choice(torch.tensor([-1, 1]))
-        remapping_curve = smoothed_noise + self.linear_factor * linear_curve_random_factor * torch.arange(256)
+        remapping_curve = smoothed_noise + self.linear_factor * np.random.choice([-1, 1]) * np.arange(256)
 
         # Scale the remapping curve between 0 and 255
-        remapping_curve = torch.interp(remapping_curve, torch.tensor([remapping_curve.min(), remapping_curve.max()]), torch.tensor([0, 255]))
+        remapping_curve = np.interp(remapping_curve, (remapping_curve.min(), remapping_curve.max()), (0, 255))
 
         # Create x-coordinates for the remapping curve
-        xp = torch.linspace(0, 255, 256)
+        xp = np.linspace(0, 255, 256)
 
-        # Apply intensity remapping to the 3D medical image (per channel)
-        augmented_image = torch.empty_like(image_array)
-        for c in range(image_array.shape[0]):  # Iterate over channels
-            augmented_image[c] = torch.interp(image_array[c].float(), xp, remapping_curve)
-        print('here', augmented_image.shape)
-        # # Convert augmented_image tensor to a NumPy array (since SimpleITK works with NumPy arrays)
-        # augmented_image_np = augmented_image.cpu().numpy()
+        # Apply intensity remapping to the 3D medical image numpy array (per channel)
+        image_array_np = image_array.numpy()
+        augmented_image_np = np.empty_like(image_array_np)
+        for c in range(image_array_np.shape[0]):  # Iterate over channels
+            augmented_image_np[c] = np.interp(image_array_np[c], xp, remapping_curve.astype(int))
 
-        # # Convert the NumPy array to a SimpleITK image
-        # # Ensure the order of dimensions is correct for saving, typically [D, H, W, C]
-        # sitk_image = sitk.GetImageFromArray(augmented_image_np.transpose(1, 2, 3, 0))  # Transpose from [C, D, H, W] to [D, H, W, C]
-
-        # # Save the image in the .mha format
-        # sitk.WriteImage(sitk_image, output_path)
+        # convert the massaged and intensity remapped numpy array to tensor for training.
+        augmented_image = torch.from_numpy(augmented_image_np)
         return augmented_image
 
 class nnUNetTrainer_DefaultDAAndIA(nnUNetTrainer):
@@ -310,65 +325,6 @@ class nnUNetTrainer_IAOnly(nnUNetTrainer):
             apply_probability=0.5 # NOTE: to be tuned
         ))
 
-        # transforms.append(RandomTransform(
-        #     GaussianNoiseTransform(
-        #         noise_variance=(0, 0.1),
-        #         p_per_channel=1,
-        #         synchronize_channels=True
-        #     ), apply_probability=0.1
-        # ))
-        # transforms.append(RandomTransform(
-        #     GaussianBlurTransform(
-        #         blur_sigma=(0.5, 1.),
-        #         synchronize_channels=False,
-        #         synchronize_axes=False,
-        #         p_per_channel=0.5, benchmark=True
-        #     ), apply_probability=0.2
-        # ))
-        # transforms.append(RandomTransform(
-        #     MultiplicativeBrightnessTransform(
-        #         multiplier_range=BGContrast((0.75, 1.25)),
-        #         synchronize_channels=False,
-        #         p_per_channel=1
-        #     ), apply_probability=0.15
-        # ))
-        # transforms.append(RandomTransform(
-        #     ContrastTransform(
-        #         contrast_range=BGContrast((0.75, 1.25)),
-        #         preserve_range=True,
-        #         synchronize_channels=False,
-        #         p_per_channel=1
-        #     ), apply_probability=0.15
-        # ))
-        # transforms.append(RandomTransform(
-        #     SimulateLowResolutionTransform(
-        #         scale=(0.5, 1),
-        #         synchronize_channels=False,
-        #         synchronize_axes=True,
-        #         ignore_axes=ignore_axes,
-        #         allowed_channels=None,
-        #         p_per_channel=0.5
-        #     ), apply_probability=0.25
-        # ))
-        # transforms.append(RandomTransform(
-        #     GammaTransform(
-        #         gamma=BGContrast((0.7, 1.5)),
-        #         p_invert_image=1,
-        #         synchronize_channels=False,
-        #         p_per_channel=1,
-        #         p_retain_stats=1
-        #     ), apply_probability=0.1
-        # ))
-        # transforms.append(RandomTransform(
-        #     GammaTransform(
-        #         gamma=BGContrast((0.7, 1.5)),
-        #         p_invert_image=0,
-        #         synchronize_channels=False,
-        #         p_per_channel=1,
-        #         p_retain_stats=1
-        #     ), apply_probability=0.3
-        # ))
-
         if mirror_axes is not None and len(mirror_axes) > 0:
             transforms.append(
                 MirrorTransform(
@@ -433,35 +389,45 @@ class nnUNetTrainer_IAOnly(nnUNetTrainer):
         return ComposeTransforms(transforms)
 
 
-    # def intensity_augmentation(self, image_array):
+    # def intensity_augmentation(self, image_array: torch.Tensor, **params) -> torch.Tensor:
     #     """
-    #     Apply intensity remapping augmentation to the input image array
-    #     and save the result as a new PNG image.
-    #     NOTE: ORIGINAL NUMPY IMPLEMENTATION
+    #     Apply intensity remapping augmentation to the input image tensor
+    #     and return the result as a PIL Image.
     #     """
-    #     # Generate random noise curve
-    #     random_noise = np.random.uniform(0, 255, size=256)
+    #     if len(params['apply_to_channel']) == 0:
+    #         return image_array
+        
+    #     # Generate random noise curve in PyTorch
+    #     random_noise = torch.rand(256) * 255
+    #     # Smooth the random noise curve with a moving average filter (convolution)
+    #     kernel = torch.ones(self.window_size) / self.window_size
+    #     kernel = kernel.unsqueeze(0).unsqueeze(0)  # Add batch and channel dimensions for 1D convolution at the very beginning
 
-    #     # Smooth the random noise curve with a moving average filter
-    #     kernel = np.ones(self.window_size) / self.window_size
-    #     smoothed_noise = convolve(random_noise, kernel, mode='reflect')
+    #     random_noise = random_noise.unsqueeze(0).unsqueeze(0)  # Add batch and channel dimensions
+    #     pad_size = ((kernel.size(-1) - 1) // 2)  # Calculate padding size 
+    #     print('pad size: ', pad_size)
+    #     padded_noise = F.pad(random_noise, (pad_size+1, pad_size), mode='reflect')
+    #     smoothed_noise = F.conv1d(padded_noise, kernel).squeeze()
+    #     print('original image shape', image_array.shape)
+    #     print('original noise tenshor shape: ', random_noise.shape)
+    #     print('padded noise tenshor shape: ', padded_noise.shape)
+    #     print('smoothed noise tenshor shape: ', smoothed_noise.shape)
 
     #     # Add a random linear component
-    #     linear_curve_random_factor = np.random.choice([-1, 1])
-    #     remapping_curve = smoothed_noise + self.linear_factor * linear_curve_random_factor * np.arange(256)
+    #     linear_curve_random_factor = torch.tensor(np.random.choice([-1, 1]))
+    #     remapping_curve = smoothed_noise + self.linear_factor * linear_curve_random_factor * torch.arange(256)
 
     #     # Scale the remapping curve between 0 and 255
-    #     remapping_curve = np.interp(remapping_curve, (remapping_curve.min(), remapping_curve.max()), (0, 255))
+    #     remapping_curve = self._interp(remapping_curve, torch.tensor([remapping_curve.min(), remapping_curve.max()]), torch.tensor([0, 255]))
 
     #     # Create x-coordinates for the remapping curve
     #     xp = np.linspace(0, 255, 256)
 
-    #     # Apply intensity remapping to the image
-    #     augmented_image = np.interp(image_array, xp, remapping_curve.astype(int))
-
-    #     rgb_array = np.stack((augmented_image,) * 3, axis=-1)
-
-    #     # Convert the RGB array back to an image
-    #     augmented_image = Image.fromarray(rgb_array.astype(np.uint8))
+    #     # Apply intensity remapping to the 3D medical image (per channel)
+    #     augmented_image = torch.empty_like(image_array)
+    #     for c in range(image_array.shape[0]):  # Iterate over channels
+    #         remapped_channel = np.interp(image_array[c].float().numpy(), xp, remapping_curve.numpy().astype(int))
+    #         augmented_image[c] = torch.from_numpy(remapped_channel)
+    #     print('here', augmented_image.shape)
 
     #     return augmented_image
